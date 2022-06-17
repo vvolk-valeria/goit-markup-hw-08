80f3c6661031956a86897d4c276d262ed0c2ce96
Volkova Valeria <vvolk.valeria@gmail.com>
2022-06-17 22:05:46 +0300

    мелкие изменения

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-var t;
-function up() {
-	var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-	if(top > 0) {
-		window.scrollBy(0,-100);
-		t = setTimeout('up()',20);
-	} else clearTimeout(t);
-	return false;
-}
