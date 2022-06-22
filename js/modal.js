168178cc016d5814d9c0494c77dcd5fd2aa6dd35
Volkova Valeria <vvolk.valeria@gmail.com>
2022-06-23 01:19:38 +0300

    scroll на мод.окно

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
